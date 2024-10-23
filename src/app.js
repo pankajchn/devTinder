625f6721caec50d8731db15b09cb9e8f53b760b7
Pankaj <realpankajchouhan08@gmail.com>
2024-10-23 20:24:05 +0530

    Handling multiple routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Pankaj", lastName: "Chouhan" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!!");
+    next();
+    // res.send("Response!!");
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!!");
+    // res.send("2nd Response!!");
+    next()
+  },
+  (req, res, next) => {
+    console.log("Handleing the router user 3!!!");
+    // res.send("3rd response!!");
+    next()
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!!!");
+    // res.send("4th response!!");
+    next()
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 5!!!");
+    res.send("5th response!!");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is succesfully listening on port 7777...");
