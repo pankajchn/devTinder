ac1e2b8d694bff0492755406575e6b44c1d7634b
Pankaj <realpankajchouhan08@gmail.com>
2024-12-03 22:09:44 +0530

    Play with route handlers and use next function

--- src/app.js
+++ src/app.js
@@ -2,20 +2,32 @@ const express = require("express");
 
 const app = express();
 
-// app.get(/.*fly$/, function (req, res) {
-//   res.send({ firstName: "Pankaj", lastName: "Chouhan" });
-// });
-
-// app.get("/user", function (req, res) {
-//   console.log(req.query);
-//   res.send({ fName: "Pankaj", lName: "Chouhan" });
-// });
-
-app.get("/user/:userId/:password/:city", function (req, res) {
-  console.log(req.params);
-  res.send({ fName: "Pankaj", lName: "Chouhan" });
-});
+app.use(
+  "/user",
+  function (req, res, next) {
+    next();
+    // res.send("Response!!!");
+    console.log("Handling the route user");
+  },
+  [
+    function (req, res, next) {
+      // res.send("Response 2");
+      console.log("Handling the route user 2");
+      next();
+    },
+  ],
+  function (req, res, next) {
+    console.log("Handling the route user 3");
+    next();
+    res.send("Response 3");
+  },
+  function (req, res,next) {
+    console.log("Handling the route user 4");
+    next()
+    res.send("Response 4");
+  }
+);
 
 app.listen(3000, function () {
-  console.log("Server is successfully running on port 3000...");
+  console.log("Server is successfully listening on port 3000");
 });
