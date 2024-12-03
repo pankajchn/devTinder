b360d088f6f62be461ca86a2474d717b30ec3b47
Pankaj <realpankajchouhan08@gmail.com>
2024-12-03 12:05:24 +0530

    Explored get, post, put and delete http methods

--- src/app.js
+++ src/app.js
@@ -2,29 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", function(req,res){
-    res.send("Breaking bad!")
-})
+// app.use("/user", function (req, res) {
+//   res.send("Breaking bad!");
+// });
 
-app.use("/home", function(req,res){
-    res.send("This is my Home Page")
-})
-
-app.use("/about", function(req,res){
-    res.send("This is our About Us page")
-})
-
-app.use("/service", function(req,res){
-    res.send("This is our Services Page")
-})
-
-app.use(function (req, res) {
-  res.send("Hello from the server");
+app.get("/user", function (req, res) {
+  res.send({ firstName: "Pankaj", lastName: "Chouhan" });
 });
 
+app.post("/user", function (req, res) {
+  res.send("Data successfully sent to the database");
+});
 
+app.delete("/user", function (req, res) {
+  res.send("Data successfully deleted from the database");
+});
 
+app.put("/user", (req, res) => {
+  res.send("Data successfully updated ");
+});
 
-app.listen(3000, function(){
-    console.log("Server is successfully running on port 3000...")
+app.listen(3000, function () {
+  console.log("Server is successfully running on port 3000...");
 });
