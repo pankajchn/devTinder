37ba913cef4a4ca4afb401a04b4051aeeb9f8e05
Pankaj <realpankajchouhan08@gmail.com>
2024-12-04 19:46:21 +0530

    Created multiple route handler with the same route.

--- src/app.js
+++ src/app.js
@@ -2,32 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  function (req, res, next) {
-    next();
-    // res.send("Response!!!");
-    console.log("Handling the route user");
-  },
-  [
-    function (req, res, next) {
-      // res.send("Response 2");
-      console.log("Handling the route user 2");
-      next();
-    },
-  ],
-  function (req, res, next) {
-    console.log("Handling the route user 3");
-    next();
-    res.send("Response 3");
-  },
-  function (req, res,next) {
-    console.log("Handling the route user 4");
-    next()
-    res.send("Response 4");
-  }
-);
+app.get("/user", function(req,res,next){
+  console.log("Handling the route user 2")
+  next()
+})
+
+app.get("/user", function(req,res,next){
+  console.log("Handling route user 1")
+  next()
+  res.send("This is Response")
+})
+
+
 
 app.listen(3000, function () {
-  console.log("Server is successfully listening on port 3000");
+  console.log("Server is running on port 3000");
 });
