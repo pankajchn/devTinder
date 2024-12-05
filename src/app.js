fa00beb5d95b664e5752ba75441c50e7a6f06fbe
Pankaj <realpankajchouhan08@gmail.com>
2024-12-05 19:02:58 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
 const express = require("express");
-const { adminAuth } = require("./middlewares/auth.js");
+const {adminAuth, userAuth} = require("./middlewares/auth")
 
 const app = express();
-
-// Middleware
-// app.use("/admin", function (req, res, next) {
-//   const token = 12460;
-//   if (token !== 12466) {
-//     res.status(401).send("Unauthorized request found");
-//   } else {
-//     next();
-//   }
-// });
 
 app.use("/admin", adminAuth);
 
@@ -20,28 +10,17 @@ app.get("/admin/getData", function (req, res) {
 });
 
 app.get("/admin/deleteUser", function (req, res) {
-  res.send("Deleted a user successfully");
+  res.send("Successfully deleted a user");
 });
 
-// app.get("/admin/getAllData", function (req, res) {
-//   const token = "xyzabc";
-//   const isAdminAuthorized = token === "xyz";
-//   if (isAdminAuthorized) {
-//     res.send("All data have sent");
-//   } else {
-//     res.status(401).send("Unauthorized request");
-//   }
-// });
+app.post("/user/login", function(req,res){
+  // database logic write here
+  res.send("User has logged in successfully")
+})
 
-// app.get("/admin/deleteUser", function (req, res) {
-//   const token = "abcxyz";
-//   const isAdminAuthorized = token === "abc";
-//   if (isAdminAuthorized) {
-//     res.send("Deleted user successfully");
-//   } else {
-//     res.status(401).send("Unauthorized request");
-//   }
-// });
+app.get("/user", userAuth, function(res,res){
+  res.send("User's data sent successfully")
+})
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,17 @@
 const adminAuth = function (req, res, next) {
   const token = 12466;
-  if (token !== 12466) {
+  const isAdminAuthorized = token === 12466;
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
+    next();
+  }
+};
+
+const userAuth = function (req, res, next) {
+  const token = 96361;
+  const isUserAuthorized = token === 9636;
+  if (!isUserAuthorized) {
     res.status(401).send("Unauthorized request found");
   } else {
     next();
@@ -9,4 +20,5 @@ const adminAuth = function (req, res, next) {
 
 module.exports = {
   adminAuth,
+  userAuth
 };
